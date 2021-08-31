31714a96b579c9df9437be31621d1eabad19d5fc
Kufre Udom <kufrekus4@gmail.com>
2021-08-31 13:50:28 +0100

    mathobj

--- datatypes.js
+++ datatypes.js
@@ -1,1 +1,30 @@
 //code along
+//PRIMITIVE
+
+//string
+const name = 'Kufre Udom';
+//number
+const age = 45;
+//boolean
+const hasKids = true;
+//Null
+const car = null;
+//undefined
+let test;
+//symbol
+const sym = Symbol();
+
+//REFERENCE TYPES - Objects
+
+//Array 
+const hobbies = ['movies', 'music'];
+//object literal
+const address = {
+    city: 'Boston',
+    state: 'MA'
+}
+const today = new Date();
+console.log(today);
+
+
+console.log(typeof(today));
