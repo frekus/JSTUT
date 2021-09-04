0d1639673e896d2c652a3b47ad912f523e4ef42d
UFOK UDOM <kufrekus4@gmail.com>
2021-09-04 22:51:56 +0100

    ifstatements

---
+++ ifstatements.js
@@ -0,0 +1,33 @@
+const id = 100;
+
+// //EQUAL TO
+// if(id == 100){
+//     console.log('CORRECT');
+// }else {
+//     console.log('INCORRECT');
+// }
+
+// //NOT EQUAL TO
+// if(id != 100){
+//     console.log('CORRECT');
+// }else {
+//     console.log('INCORRECT');
+// }
+
+// //EQUAL TO VALUE AND TYPE
+// if(id === 101){
+//     console.log('CORRECT');
+// }else {
+//     console.log('INCORRECT');
+// }
+
+// //NOT EQUAL TO VALUE AND TYPE
+// if(id !== 101){
+//     console.log('CORRECT');
+// }else {
+//     console.log('INCORRECT');
+// }
+
+if(id){
+    console.log(`The ID is ${id}`);
+}
