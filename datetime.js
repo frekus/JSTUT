c57b847192af2692cf60a106b824c780706b0fd2
UFOK UDOM <kufrekus4@gmail.com>
2021-09-04 21:55:29 +0100

    datetime

---
+++ datetime.js
@@ -0,0 +1,9 @@
+let val;
+const today = new Date();
+let birthday = new Date('9-10-1981 11:25:00');
+birthday = new Date('September 20 1981');
+birthday = new Date('9/20/1981')
+
+val = birthday;
+
+console.log(val);
