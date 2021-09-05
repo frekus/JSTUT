81d5f945a89d4b00595b252c63dc39a19d1f4de8
UFOK UDOM <kufrekus4@gmail.com>
2021-09-05 02:47:03 +0100

    ifstatement

--- ifstatements.js
+++ ifstatements.js
@@ -29,8 +29,57 @@ const id = 100;
 // }
 
 //test if undefined
-if(typeof id !== 'undefined'){
-    console.log(`The ID is ${id}`);
-}else {
-    console.log('NO ID');
+// if(typeof id !== 'undefined'){
+//     console.log(`The ID is ${id}`);
+// }else {
+//     console.log('NO ID');
+// }
+
+//GREATER OR LESS THAN
+// if(id >= 200){
+//     console.log('CORRECT');
+// } else {
+//     console.log('INCORRECT');
+
+// }
+
+//IF ELSE
+
+const color = 'yellow';
+
+// if(color === 'red'){
+//     console.log('color is red');
+// } else if(color === 'blue'){
+//     console.log('Color is blue');
+// } else {
+//     console.log('Color is not red or blue');
+// }
+
+//LOGICAL OPERATORS
+
+//AND &&
+const name = 'Steve';
+const age = 40;
+if(age > 0 && age < 12){
+    console.log(`${name} is a child`);
+} else if(age >=13 && age <= 19){
+    console.log(`${name} is a teenager`);
+} else {
+    console.log(`${name} is an adult`);
 }
+
+//OR ||
+if(age < 16 || age > 65){
+    console.log(`${name} cannot run in race`);
+} else {
+    console.log(`${name} is registered for the race`);
+}
+
+//TERNARY OPERATOR
+console.log(id === 100 ? 'CORRECT' : 'INCORRECT');  
+
+//WITHOUT BRACES
+if(id === 100)
+    console.log('CORRECT');
+else
+    console.log('INCORRECT');
