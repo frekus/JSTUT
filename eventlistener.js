00f9b9bfd95aeea4e47810b355d5cdceafbd6699
UFOK UDOM <kufrekus4@gmail.com>
2021-10-02 17:37:42 +0100

    refresh

---
+++ eventlistener.js
@@ -0,0 +1,16 @@
+// document.querySelector('.clear-tasks').addEventListener('click',
+// function(e){
+//     console.log('Hello world');
+//     e.preventDefault();
+// });
+
+document.querySelector('.clear-tasks').addEventListener('click', onClick);
+
+function onClick(e){
+    // console.log('Clicked');
+    let val;
+
+    val = e;
+
+    console.log(val);
+}
