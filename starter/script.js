1b4dedc025af766e87c46bef1931a04fbc86829e
udomk <kufrekus4@gmail.com>
2024-03-30 21:11:42 +0100

    onward

--- starter/script.js
+++ starter/script.js
@@ -124,4 +124,58 @@ const interestedIn = prompt('What do you want to know about Jonas? Choose betwee
 // Challenge
 //"Jonas is a 46-year old teacher, and he has a/no driver's license"
 // console.log(jonas.getSummary());
-    
+
+//for loop keeps running while the condition is true
+/* for (let rep = 1; rep <= 10; rep++){
+    console.log(`lifting weights repetition ${rep} 🏋️‍♀️`)
+} */
+
+const jonas = [
+    'Jonas',
+    'Schmedtmann',
+    2037 - 1991,
+    'teacher',
+    ['Michael', 'Peter', 'Steven'],
+    true
+];
+
+/* for(let i = 0; i < jonas.length; i++) {
+    console.log(jonas[i], typeof jonas[i]);
+} */
+
+/* const years = [1991, 2007, 1969, 2020];
+const ages = [];
+
+for (let i =0; i < years.length; i++) {
+    ages.push(2037 - years[i]);
+}
+console.log(ages); */
+
+//continue and break
+/* console.log('-----ONLY STRINGS-----')
+
+for (let i = 0; i < jonas.length; i++) {
+    if(typeof jonas[i] !== 'string') continue;
+
+    console.log(jonas[i], typeof jonas[i]);
+}
+
+console.log('-----BREAK WITH NUMBER-----')
+for (let i = 0; i < jonas.length; i++) {
+    if(typeof jonas[i] === 'number') break;
+
+    console.log(jonas[i], typeof jonas[i]);
+} */
+/* for (let i = jonas.length - 1; i >= 0; i--) {
+    console.log(i, jonas[i]);
+} */
+
+//loop in a loop
+for (let exercise = 1; exercise < 4; exercise++) {
+  console.log(`------- Starting Exercise ${exercise}`);
+
+
+for (let rep = 1; rep < 6; rep++) {
+    console.log(`Exercise ${exercise}:Lifting weight repetition ${rep} 🏋️‍♀️`)  ;
+    }
+}
