0ab4f7b30576afe7d796dd5be68d1d66d899fa89
udomk <kufrekus4@gmail.com>
2024-03-30 14:12:53 +0100

    JS 24

--- starter/script.js
+++ starter/script.js
@@ -39,7 +39,7 @@ const appleJuice = fruitProcessor(2,4);
 console.log(appleJuice) */
 
 //function declaration
-function calcAge1(birthYear) {
+/* function calcAge1(birthYear) {
     return 2037 - birthYear;
 }
 
@@ -51,4 +51,45 @@ const calcAge2 = function (birthYear) { //anonymous function
 }
 
 const age2 = calcAge2(1991);
-console.log(age1, age2);
+console.log(age1, age2); */
+
+// Arrow function
+/* const calcAge3 = birthYear => 2037 - birthYear;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = 2037 - birthYear;
+    const retirement = 65 - age;
+    //return retirement;
+    return `${firstName} retires in ${retirement} years`;
+}
+
+console.log(yearsUntilRetirement(1991, 'Jonas'));
+console.log(yearsUntilRetirement(1980, 'Bob')); */
+
+//Functions calling other functions
+
+const jonas = {
+    firstName: 'Jonas',
+    lastName: 'Kuf',
+    age: 2037 - 1991,
+    job: 'teacher',
+    friends: ['Michael', 'Peter', 'Steven']
+};
+console.log(jonas);
+
+console.log(jonas.lastName);
+console.log(jonas['lastName']);
+
+const nameKey = 'Name';
+console.log(jonas['first' + nameKey])
+console.log(jonas['last' + nameKey])
+
+const interestedIn = prompt('What do you want to know about Jonas? Choose between firstName.....')
+
+if (jonas[interestedIn]) {
+    console.log(jonas[interestedIn]);
+} else {
+    console.log('Wrong request, Choose between .......')
+}
